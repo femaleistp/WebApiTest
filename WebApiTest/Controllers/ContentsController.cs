2dadbb25480eb889b60670062b7532f2052372f4
Jesse Harlan <jesse.harlan@centralia.edu>
2025-02-18 10:30:17 -0800

    added claims controller

--- WebApiTest/Controllers/ContentsController.cs
+++ WebApiTest/Controllers/ContentsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApiTest.Data;
